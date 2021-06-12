574db0f13d6c8394689e6249293c193b52bcba01
charbel <charbel-boumaroun@outlook.com>
2021-06-12 13:16:56 +0200

    Added db connection

---
+++ db.js
@@ -0,0 +1,48 @@
+const { Pool } = require('pg')
+const dotenv = require("dotenv");
+dotenv.config();
+console.log(process.env.CONNECTON_STRING)
+var conString = process.env.CONNECTON_STRING;
+
+const pool = new Pool({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+  });
+
+
+const execute = async (query) => {
+    try {
+        await pool.query(query);  // sends queries
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    }
+};
+
+const text = `
+    CREATE TABLE IF NOT EXISTS "traffic" (
+	    "id" SERIAL,
+	    "start" VARCHAR(200) NOT NULL,
+	    "destination" VARCHAR(200) NOT NULL,
+        "duration" integer,
+        "time" TIMESTAMP,
+	    PRIMARY KEY ("id")
+    );`;
+
+execute(text).then(result => {
+    if (result) {
+        console.log('Table created');
+    }
+});
+
+
+module.exports ={
+    execute
+}
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 const readline = require('readline');
-
+const db = require('./db')
 
 let running = false;
 console.log("Press 'q' to exit the program, press 'l' to start the program.")
@@ -35,40 +35,53 @@ process.stdin.on('keypress', (str, key) => {
 
 async function scrape() {
     const browser = await puppeteer.launch({
-        "headless": true,
+        "headless": false,
         "args": ["--fast-start", "--disable-extensions", "--no-sandbox"],
         "ignoreHTTPSErrors": true
     });
-
-    const page = await browser.newPage();
-    await page.setJavaScriptEnabled(true)
-    await page.goto('https://www.google.com/maps/dir/Lebanese+American+University,+Byblos+Campus,+Blat,+Lebanon/Fanar,+Lebanon/@34.0010134,35.4776456,11z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x151f5b485f22ba8b:0xb777d642de56b49a!2m2!1d35.6744347!2d34.1155614!1m5!1m1!1s0x151f3d919385703f:0xcdd985b12eddd900!2m2!1d35.578307!2d33.8798483!3e0');
-    await page.click("#yDmH0d > c-wiz > div > div > div > div.NIoIEf > div.G4njw > div.AIC7ge > form > div.lssxud > div > button > span");
-    await page .waitForSelector('.section-directions-trip-duration');
-
-    //await new Promise(resolve => setTimeout(resolve, 5000));
-    while (true && running) {
-        let minute = await page.evaluate(() => {
-            let minuteElement = document.body.querySelector("#section-directions-trip-0 > div > div:nth-child(1) > div.section-directions-trip-numbers > div.section-directions-trip-duration.delay-medium > span:nth-child(1)").textContent
-            return minuteElement
-        });
-        
-        //Main logic 
-        console.log(minute);
-
-
-
-        //End main logic
-
-
-
-        await page.reload();
-        await new Promise(resolve => setTimeout(resolve, 5000));
-        if(!running){
-            await browser.close()
+    try{
+        const page = await browser.newPage();
+        await page.setJavaScriptEnabled(true)
+        await page.goto('https://www.google.com/maps/dir/Lebanese+American+University,+Byblos+Campus,+Blat,+Lebanon/Fanar,+Lebanon/@34.0010134,35.4776456,11z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x151f5b485f22ba8b:0xb777d642de56b49a!2m2!1d35.6744347!2d34.1155614!1m5!1m1!1s0x151f3d919385703f:0xcdd985b12eddd900!2m2!1d35.578307!2d33.8798483!3e0');
+        await page.click("#yDmH0d > c-wiz > div > div > div > div.NIoIEf > div.G4njw > div.AIC7ge > form > div.lssxud > div > button > span");
+        await page .waitForSelector('.section-directions-trip-duration');
+    
+        while (true && running) {
+            let minute = await page.evaluate(() => {
+                let minuteElement = document.body.querySelector(".section-directions-trip-duration").textContent
+                
+                return minuteElement
+            });
+            
+            //Main logic 
+            var regex = /\d+/g;
+            var numbers = parseInt(minute.match(regex)[0]);  // creates array from matches
+            if(minute.indexOf('min')!=-1){
+                duration = numbers
+            }else{
+                duration = numbers * 60
+            }
+            console.log(duration);
+            date = new Date().toISOString();
+            console.log(date)
+            await db.execute(`INSERT INTO traffic (id, start, destination, duration, time) VALUES (DEFAULT, 'jdeideh', 'jbeil', 30, '${date}')`)
+           
+            //End main logic
+    
+    
+    
+            await page.reload();
+            await new Promise(resolve => setTimeout(resolve, 5000));
+            if(!running){
+                await browser.close()
+            }
         }
+       console.log("Program closed ...")
+    }catch(error){
+        console.log("Error caught, closing browser ...")
+        await browser.close()
     }
-   console.log("Program closed ...")
+   
 
 
 
