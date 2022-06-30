52a3d31ed3e123552a13c730a80a13f0597861e8
Charbel199 <charbel-boumaroun@outlook.com>
2022-06-30 16:38:16 +0200

    Update project

--- main.js
+++ main.js
@@ -10,13 +10,6 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
 
-const swaggerUi = require('swagger-ui-express'),
-swaggerDocument = require('./swagger.json');
-app.use(
-  '/api-docs',
-  swaggerUi.serve, 
-  swaggerUi.setup(swaggerDocument)
-);
 
 let browsers = [];
 //For development
